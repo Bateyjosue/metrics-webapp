c45279592c82281c7b3cb4a104614eaf6e224d1f
Bateyjosue <josuebatey19@gmail.com>
2022-10-10 15:16:48 +0200

    Create Fetch Books Reducer and Action

---
+++ src/Redux/Books/reducerBooks.js
@@ -0,0 +1,20 @@
+const BASE_URL = 'https://the-dune-api.herokuapp.com/books/15';
+const GET_DUME_BOOK = 'GET_DUME_BOOK';
+const stateInit = [];
+
+const reducerDukeBooks = (state=stateInit, actions) => {
+  switch(actions.type){
+    case GET_DUME_BOOK: return [...state, ...actions.payload]
+    default: return state;
+  }
+}
+
+export const getDumeBook = createAsyncThunk(
+  GET_DUME_BOOK,
+  async () => {
+    const resp = await axios.get(BASE_URL);
+    return resp.data;
+  }
+);
+
+export default  reducerDukeBooks
