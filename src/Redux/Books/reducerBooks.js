1726afb16bed54fd47c130e9c4c2aa8bd141a5e7
Bateyjosue <josuebatey19@gmail.com>
2022-10-11 12:14:55 +0200

    Change the API baseURL and response data format

--- src/Redux/Books/reducerBooks.js
+++ src/Redux/Books/reducerBooks.js
@@ -1,22 +1,36 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-const BASE_URL = 'https://the-dune-api.herokuapp.com/books/15';
+
+const BASE_URL = 'https://bobsburgers-api.herokuapp.com/characters/?limit=20&skip=489';
 const GET_DUME_BOOK = 'GET_DUME_BOOK';
 const stateInit = [];
 
-const reducerDukeBooks = (state=stateInit, actions) => {
-  switch(actions.type){
-    case `${GET_DUME_BOOK}/fulfilled`: return [...state, ...actions.payload]
+const reducerDukeBooks = (state = stateInit, action) => {
+  switch (action.type) {
+    case `${GET_DUME_BOOK}/fulfilled`: return [...state, ...action.payload];
     default: return state;
   }
-}
+};
 
 export const getDumeBook = createAsyncThunk(
   GET_DUME_BOOK,
   async () => {
     const resp = await axios.get(BASE_URL);
-    return resp.data;
-  }
+    const data = await resp.data.map((book) => ({
+      id: book.id,
+      name: book.name,
+      image: book.image,
+      gender: book.denger ? book.denger : null,
+      age: book.age ? book.age : null,
+      hairColor: book.hairColor ? book.hairColor : null,
+      wikiUrl: book.wikiUrl,
+      relatives: book.relatives ? book.relatives : [],
+      occupation: book.occupation ? book.occupation : null,
+      firstEpisodes: book.firstEpisodes ? book.firstEpisodes : null,
+      voiceBy: book.voiceBy ? book.voiceBy : null,
+    }));
+    return data;
+  },
 );
 
-export default  reducerDukeBooks
+export default reducerDukeBooks;
